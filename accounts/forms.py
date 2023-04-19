3a55d8ae1b826ee009ff1ab0082eebc2d8afa902
Park <angle307@naver.com>
2023-04-19 16:56:18 +0900

    [ADD] accounts signup

---
+++ accounts/forms.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django import forms
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '아이디'
+            }
+        ),
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '이메일'
+            }
+        ),
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '비밀번호'
+            }
+        ),
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '비밀번호 확인'
+            }
+        ),
+    )
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        field = ('username', 'email', 'password1', 'password2',)
